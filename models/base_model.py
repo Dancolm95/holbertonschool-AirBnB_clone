c1cb62a19d2adad09698e3265950c6c81fec29d3
dancolm95 <dancolm95@gmail.com>
2023-10-30 19:19:55 -0700

    class BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self)
+    def __init__(self, id, created_at, updated_at)
 
     tform = "%Y-%m-%dT%H:%M:%S.%f"
     self.id = str(uuid4())
