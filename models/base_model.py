70167d4113b7d526012954aab98ad28f91665c55
dancolm95 <dancolm95@gmail.com>
2023-11-01 09:57:48 -0700

    new try

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,60 @@
-#!/usr/bin/bash
-"""Define the Basemodel class"""
-
+#!/usr/bin/python3
+"""
+Este modulo representa una clase BaseModel.
+"""
 import uuid
 from datetime import datetime
 from models import storage
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        """
+        Inicializa una instancia de BaseModel.
 
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
+        Atributos:
+
+        Id: Genera un ID único utilizando uuid4 y
+        lo convierte en una cadena.
+
+        Created_at: Establece la fecha y hora de creación
+        al momento actual en formato ISO.
+
+        Updated_at: Establece la fecha y hora de actualización
+        al momento actual en formato ISO.
+
+        Args:
+            **Kwargs: Resibe un diccionario:
+        """
+        if kwargs:
             for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
+                if k != "__class__":
+                    setattr(self, k, v)
+            self.__dict__["created_at"] = datetime.strptime(
+                self.__dict__["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+
+            self.__dict__["updated_at"] = datetime.strptime(
+                self.__dict__["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+
         else:
-            models.storage.new(self)
-
-    def save(self):
-        self.updated_at = datetime.today()
-        models.storage.save()
-
-    def to_dict(self):
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.created_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}",format(clname, self.id, self.__dict__)
+        """Devuelve una representación de cadena del objeto."""
+        return f"[BaseModel] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Actualiza el atributo updated_at con la fecha y hora actual."""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Devuelve un diccionario con los atributos del objeto."""
+        my_dict = dict(self.__dict__)
+        my_dict['created_at'] = self.__dict__['created_at'].isoformat()
+        my_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
+        my_dict['__class__'] = self.__class__.__name__
+        return my_dict
