3bc1496941dd7f8b6e253cc427b12f3f1448888e
root <dancolm95@gmail.com>
2023-11-09 21:45:39 -0800

    new class amenity

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Define the class 'Amenity'.
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represent the service.
+
+    Attributes:
+        name (str): The name of service.
+    """
+
+    name = ""
