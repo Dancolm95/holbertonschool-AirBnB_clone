2b1cd3d6e460640747b7e11a4ad6318926ac8a66
root <dancolm95@gmail.com>
2023-11-09 21:46:34 -0800

    new class state

---
+++ models/state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Define the class 'state'.
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represent a State.
+
+    Attributes:
+        name (str): The name of State.
+    """
+
+    name = ""
